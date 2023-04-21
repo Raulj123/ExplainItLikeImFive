a599ee156f51884c3dacacaf5f2bfc921e6659cb
raul <jarquinr121@gmail.com>
2023-04-21 16:54:51 -0700

    on: input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,29 @@
+<script lang="ts">
+ 
+function displayText(){
+	const explanation = document.getElementById("para") as HTMLParagraphElement;
 
+	var getText = document.getElementById("content") as HTMLTextAreaElement;
+	if(getText !== null){
+
+		console.log(getText.value)
+		explanation.textContent = getText.value;
+	}
+	
+	
+}
+
+
+</script>
 
 
 
 <form>
-	<textarea name="content"rows="7" class="flex mx-auto w-[40%] "></textarea>
-	<button type="button" class="btn flex mx-auto  m-4 w-[40%] variant-filled-primary py-2">Explain It</button>
+	<textarea id="content"rows="7" class="flex mx-auto w-[40%] " placeholder="hello?" on:input={()=>displayText()}></textarea>
+	<button type="button" class="btn flex mx-auto  m-4 w-[40%] variant-filled-primary py-2" >Explain It</button>
 	<div class="m-10">
 		<h2 class="font-bold ml-40 text-left">Explanation:</h2>
-		<p class="text-center">text here</p>
+		<p id="para"class="text-center"></p>
 	</div>
 </form>
 
